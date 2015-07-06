b01d35f8c71a208dca77d377ab5de3692c630997
gongming <gongmingqm10@foxmail.com>
2015-07-06 21:37:05 +0800

    Ming - Add RegisterViewController to Storyboard

---
+++ DriftReading/DriftReading/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  DriftReading
+//
+//  Created by Ming Gong on 7/6/15.
+//  Copyright © 2015 gongmingqm10. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: ViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ DriftReading/DriftReading/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  DriftReading
+//
+//  Created by Ming Gong on 7/6/15.
+//  Copyright © 2015 gongmingqm10. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: ViewController {
+    override func viewDidLoad() {
+        
+    }
+}
